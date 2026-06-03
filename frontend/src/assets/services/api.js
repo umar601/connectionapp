fdeb56676f043d5694c639707add713d03ca548c
umar abbas <umarshah6444@gmail.com>
2026-06-03 13:24:20 +0500

    frontend connected with backedn

---
+++ frontend/src/assets/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:"http://localhost:8080",
+    headers:{"Content-Type":"application/json"}
+
+})
+
+export const getPost = ()=>{ 
+    return api.get("/post")
+};
+
+

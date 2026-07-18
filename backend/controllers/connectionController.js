8d00a85b162ba6c07579c9cd15d5db82f07dfdfb
umar abbas <umarshah6444@gmail.com>
2026-07-18 22:33:13 +0500

    update

--- backend/controllers/connectionController.js
+++ backend/controllers/connectionController.js
@@ -20,10 +20,15 @@ let sendRequest = async (req,res)=>{
 
     try{
 
+        // console.log(req.body.userId)
+        // console.log(req.body.sendTo)
         //finding the sender and reciever 
 
     let sendBy = await userModel.findById(req.body.userId);
     let sendTo = await userModel.findById(req.body.sendTo);
+
+    // console.log(sendBy)
+    // console.log(sendTo)
 
         //checing weather they exist 
 
@@ -37,21 +42,22 @@ let sendRequest = async (req,res)=>{
 
     let isRequestFound = await connectionModel.findOne(
     
-        {sendTo:sendTo._id},
-        {sendBy:sendBy._id}
+        {sendTo:sendTo._id,
+        sendBy:sendBy._id}
     )
 
+    
     // console.log(isRequestFound)
 
     if(isRequestFound){
 
-        return res.status(500).json({message:"request already exist "});
+        return res.status(404).json({message:"request already exist "});
     }
 
 
     await connectionModel.insertOne(
         {
-            SendBy:sendBy,
+            sendBy:sendBy,
             sendTo:sendTo
         }
     )
@@ -211,9 +217,13 @@ let seeSendRequest = async(req,res)=>{
 
     try{
 
+    // console.log("Working")
+
     let sendBy = await userModel.findById(req.params.userId);
 
     //checing weather they exist 
+
+    // console.log(sendBy)
 
     if(!sendBy){
 
@@ -226,11 +236,11 @@ let seeSendRequest = async(req,res)=>{
         
         {sendBy:sendBy._id,
         status:"pending"}
-    )
+    ).populate("sendTo")
 
     // console.log(fetchedRequests)
 
-    res.status(200).json({message:"fecthed sucessfully ",requests:fetchedRequests})
+    return res.status(200).json({message:"fecthed sucessfully ",requests:fetchedRequests})
 
 
     }catch(err){
@@ -258,7 +268,7 @@ let seeRequestRecieved = async(req,res)=>{
 
         let fetchedRequests = await connectionModel.find({
             sendTo:req.params.userId,
-            status:"pending"});
+            status:"pending"}).populate("sendBy")
 
         res.status(200).json({message:"fecthed sucessfully ",requests:fetchedRequests})
 
@@ -276,14 +286,16 @@ let seeConnections = async(req,res)=>{
 
     try{
 
-        let sendBy = await userModel.findById(req.params.userId);
+        let sendBy = await userModel.findById(req.params.userId)
 
         if(!sendBy){
 
         return res.status(500).json({message:"something wrong in "});
     }
 
-    let connections = sendBy.connections;
+    let connections = sendBy.connections
+
+    console.log(connections)
 
     res.status(200).json({message:"fecthec successfully ",connections:connections})
 

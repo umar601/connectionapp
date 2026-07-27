865a6e7b1ddb6e5140090f1e6f01314e44c3efff
umar abbas <umarshah6444@gmail.com>
2026-07-28 00:48:37 +0500

    up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,200 +1,332 @@
-//sign up
-//login
+// //sign up
+// //login
 
+// const userModel = require("../models/userModel");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+
+// const userSignup = async (req,res)=>{  
+    
+//     //automatically login after signup thats why we are generating token here 
+
+
+//     //checking account already exist or not 
+
+//     try{
+
+//     let isAccountFound = await userModel.findOne({username:req.body.username});
+
+//     //if account is not found then 
+
+//     if(!isAccountFound){ 
+
+//         const salt = await bcrypt.genSalt(10);
+//         const hashPassword  = await bcrypt.hash(req.body.password,salt);
+
+
+//         //creating account
+
+//         let newUser  = new userModel(
+//             {
+//                 username:req.body.username,
+//                 password:hashPassword
+//             }
+//         )
+
+//         await newUser.save();
+
+//         //generating token data 
+
+//         let tokenData = {
+//             id : newUser._id,
+//             username: newUser.username
+//         }
+
+//         //generating token 
+
+//         let generatedToken = jwt.sign(
+//             {
+//                 data:tokenData
+
+//             },
+//             "secretKey",
+//             {expiresIn:"1h"}
+//         )
+
+//         //storing token in cookie
+
+//         res.cookie("token",generatedToken,{
+//             httpOnly:true,
+//             maxAge:2*60*1000 
+//         })
+
+//         //storing user in toknen
+
+//         res.cookie("currentUser",newUser,{
+//                 httpOnly:true,
+//                 maxAge:2*60*1000           
+//         })
+
+
+//         res.status(200).json({message:"account created successfully",id:newUser._id,username:newUser.username});
+
+
+//     }
+//     else{
+        
+//         res.status(409).json({message:"account already exist"});
+//     }
+
+//     }catch(err){
+//         res.status(500).json({message: "Server error", err: err.message})
+    
+//     }  
+
+
+// }
+
+
+// const userLogin  = async (req,res) => {
+
+//     try{
+
+//         // console.log(req.body.username,req.body.password)
+
+//         //first checking weather the user exist or not 
+
+//         let isUserFound = await userModel.findOne({username:req.body.username});
+
+//         // console.log(isUserFound)
+
+//         //if user exist 
+
+
+//         if(isUserFound){
+
+//             //matching the password 
+
+//             let isPasswordMatched = await bcrypt.compare(req.body.password,isUserFound.password);
+
+//             if(isPasswordMatched){
+        
+
+//             //making the token 
+
+//             let tokenData = {
+//                 id:isUserFound._id,
+//                 username:req.body.username
+//             }
+
+
+//             //generating token 
+
+
+//             let generatedToken = jwt.sign(
+//                 {
+//                     data:tokenData
+//                 },
+//                 "secretKey",
+//                 {
+//                     expiresIn:"1h"
+//                 }
+//             )
+
+//             //storing token in cookie 
+
+//             res.cookie("token",generatedToken,{
+//                 httpOnly:true,
+//                 maxAge:2*60*1000            
+//             })
+
+//             //storing user in cookie
+
+//             res.cookie("currentUser",isUserFound,{
+//                 httpOnly:true,
+//                 maxAge:2*60*1000        
+//             })
+
+           
+//             // login succesfull
+
+//             res.status(200).json({message:"user login successful",token:generatedToken,id:isUserFound._id,username:isUserFound.username});
+
+//             }else{
+
+//             res.status(401).json({message:"password is wrong"});
+
+//             }
+            
+//         }else{
+
+//             res.status(404).json({message:"user not found"});
+
+
+//         }
+
+
+//     }catch(err){
+//         res.status(500).json({message:"some error in login ",error:err});
+//     }
+    
+// }
+
+// //this is used to get the login user 
+
+// const getLoginUser  = async ( req,res) =>{
+
+//     try {
+
+//         let loginUser = await userModel.findById(req.loginUser._id).select("-password"); //fetch user but not his pasword 
+
+//         if(loginUser){
+
+//             res.status(200).json({messgae:"user found",user:loginUser});
+//         }else{
+
+//             res.status(404).json({message:"user not found"});
+//         }
+
+
+//     }catch(err){
+
+//         res.status(500).json({message:"some error occured ",err:err});
+//     }
+// }
+
+
+// module.exports = { userSignup, userLogin,getLoginUser}; 
+
+
+// authController.js - FIXED VERSION
 const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-const userSignup = async (req,res)=>{  
-    
-    //automatically login after signup thats why we are generating token here 
+const userSignup = async (req, res) => {
+    try {
+        let isAccountFound = await userModel.findOne({ username: req.body.username });
 
+        if (!isAccountFound) {
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    //checking account already exist or not 
+            let newUser = new userModel({
+                username: req.body.username,
+                password: hashPassword
+            });
 
-    try{
-
-    let isAccountFound = await userModel.findOne({username:req.body.username});
-
-    //if account is not found then 
-
-    if(!isAccountFound){ 
-
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword  = await bcrypt.hash(req.body.password,salt);
-
-
-        //creating account
-
-        let newUser  = new userModel(
-            {
-                username:req.body.username,
-                password:hashPassword
-            }
-        )
-
-        await newUser.save();
-
-        //generating token data 
-
-        let tokenData = {
-            id : newUser._id,
-            username: newUser.username
-        }
-
-        //generating token 
-
-        let generatedToken = jwt.sign(
-            {
-                data:tokenData
-
-            },
-            "secretKey",
-            {expiresIn:"1h"}
-        )
-
-        //storing token in cookie
-
-        res.cookie("token",generatedToken,{
-            httpOnly:true,
-            maxAge:2*60*1000 
-        })
-
-        //storing user in toknen
-
-        res.cookie("currentUser",newUser,{
-                httpOnly:true,
-                maxAge:2*60*1000           
-        })
-
-
-        res.status(200).json({message:"account created successfully",id:newUser._id,username:newUser.username});
-
-
-    }
-    else{
-        
-        res.status(409).json({message:"account already exist"});
-    }
-
-    }catch(err){
-        res.status(500).json({message: "Server error", err: err.message})
-    
-    }  
-
-
-}
-
-
-const userLogin  = async (req,res) => {
-
-    try{
-
-        // console.log(req.body.username,req.body.password)
-
-        //first checking weather the user exist or not 
-
-        let isUserFound = await userModel.findOne({username:req.body.username});
-
-        // console.log(isUserFound)
-
-        //if user exist 
-
-
-        if(isUserFound){
-
-            //matching the password 
-
-            let isPasswordMatched = await bcrypt.compare(req.body.password,isUserFound.password);
-
-            if(isPasswordMatched){
-        
-
-            //making the token 
+            await newUser.save();
 
             let tokenData = {
-                id:isUserFound._id,
-                username:req.body.username
-            }
-
-
-            //generating token 
-
+                id: newUser._id,
+                username: newUser.username
+            };
 
             let generatedToken = jwt.sign(
-                {
-                    data:tokenData
-                },
+                { data: tokenData },
                 "secretKey",
-                {
-                    expiresIn:"1h"
-                }
-            )
+                { expiresIn: "7d" } // ✅ CHANGE: 1h → 7d
+            );
 
-            //storing token in cookie 
+            // ✅ FIX 1: Store token with longer expiry
+            res.cookie("token", generatedToken, {
+                httpOnly: true,
+                maxAge: 7 * 24 * 60 * 60 * 1000 // ✅ CHANGE: 2 minutes → 7 days
+            });
 
-            res.cookie("token",generatedToken,{
-                httpOnly:true,
-                maxAge:2*60*1000            
-            })
+            // ✅ FIX 2: Remove password before storing in cookie
+            const userWithoutPassword = {
+                _id: newUser._id,
+                username: newUser.username
+            };
 
-            //storing user in cookie
+            // ✅ FIX 3: Make currentUser readable by frontend
+            res.cookie("currentUser", userWithoutPassword, {
+                httpOnly: false, // ✅ CHANGE: true → false
+                maxAge: 7 * 24 * 60 * 60 * 1000 // ✅ CHANGE: 2 minutes → 7 days
+            });
 
-            res.cookie("currentUser",isUserFound,{
-                httpOnly:true,
-                maxAge:2*60*1000        
-            })
-
-           
-            // login succesfull
-
-            res.status(200).json({message:"user login successful",token:generatedToken,id:isUserFound._id,username:isUserFound.username});
-
-            }else{
-
-            res.status(401).json({message:"password is wrong"});
-
-            }
-            
-        }else{
-
-            res.status(404).json({message:"user not found"});
-
-
+            res.status(200).json({
+                message: "account created successfully",
+                id: newUser._id,
+                username: newUser.username
+            });
+        } else {
+            res.status(409).json({ message: "account already exist" });
         }
-
-
-    }catch(err){
-        res.status(500).json({message:"some error in login ",error:err});
+    } catch (err) {
+        res.status(500).json({ message: "Server error", err: err.message });
     }
-    
 }
 
-//this is used to get the login user 
-
-const getLoginUser  = async ( req,res) =>{
-
+const userLogin = async (req, res) => {
     try {
+        let isUserFound = await userModel.findOne({ username: req.body.username });
 
-        let loginUser = await userModel.findById(req.loginUser._id).select("-password"); //fetch user but not his pasword 
+        if (isUserFound) {
+            let isPasswordMatched = await bcrypt.compare(req.body.password, isUserFound.password);
 
-        if(loginUser){
+            if (isPasswordMatched) {
+                let tokenData = {
+                    id: isUserFound._id,
+                    username: req.body.username
+                };
 
-            res.status(200).json({messgae:"user found",user:loginUser});
-        }else{
+                let generatedToken = jwt.sign(
+                    { data: tokenData },
+                    "secretKey",
+                    { expiresIn: "7d" } // ✅ CHANGE: 1h → 7d
+                );
 
-            res.status(404).json({message:"user not found"});
+                // ✅ FIX 1: Store token with longer expiry
+                res.cookie("token", generatedToken, {
+                    httpOnly: true,
+                    maxAge: 7 * 24 * 60 * 60 * 1000 // ✅ CHANGE: 2 minutes → 7 days
+                });
+
+                // ✅ FIX 2: Remove password before storing in cookie
+                const userWithoutPassword = {
+                    _id: isUserFound._id,
+                    username: isUserFound.username
+                };
+
+                // ✅ FIX 3: Make currentUser readable by frontend
+                res.cookie("currentUser", userWithoutPassword, {
+                    httpOnly: false, // ✅ CHANGE: true → false
+                    maxAge: 7 * 24 * 60 * 60 * 1000 // ✅ CHANGE: 2 minutes → 7 days
+                });
+
+                res.status(200).json({
+                    message: "user login successful",
+                    // ✅ REMOVED: token from response (it's in cookie now)
+                    id: isUserFound._id,
+                    username: isUserFound.username
+                });
+            } else {
+                res.status(401).json({ message: "password is wrong" });
+            }
+        } else {
+            res.status(404).json({ message: "user not found" });
         }
-
-
-    }catch(err){
-
-        res.status(500).json({message:"some error occured ",err:err});
+    } catch (err) {
+        res.status(500).json({ message: "some error in login ", error: err });
     }
 }
 
+const getLoginUser = async (req, res) => {
+    try {
+        let loginUser = await userModel.findById(req.loginUser._id).select("-password");
+        if (loginUser) {
+            res.status(200).json({ message: "user found", user: loginUser });
+        } else {
+            res.status(404).json({ message: "user not found" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "some error occured ", err: err });
+    }
+}
 
-module.exports = { userSignup, userLogin,getLoginUser}; 
+module.exports = { userSignup, userLogin, getLoginUser };
 
 
 
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,77 +1,126 @@
-// flow of protected route in forntend 
+// // flow of protected route in forntend 
 
-// make cutom hook hook and its must be function and use some other hooke andd name must start form use 
+// // make cutom hook hook and its must be function and use some other hooke andd name must start form use 
 
-// useAuth and use empty container using create conetxt 
+// // useAuth and use empty container using create conetxt 
 
-// check weather user login or not if login set user elese not 
+// // check weather user login or not if login set user elese not 
 
-// at end export the cusytom hook to use in other 
+// // at end export the cusytom hook to use in other 
 
-// make protected route and render the page if if login else maove abck to login
+// // make protected route and render the page if if login else maove abck to login
 
-// use to store login in login 
+// // use to store login in login 
 
-// and also use in main to protect overall and app to protect specfic route 
+// // and also use in main to protect overall and app to protect specfic route 
 
-// make a route to scheck user is login untill cookie expire so user not logout when refresh 
-
-
-import { createContext,useState,useEffect,useContext } from "react";
-
-import {fetechLogin} from "../assets/services/api";   //fetching the route 
-
-const AuthContext = createContext();  //creating empty container 
+// // make a route to scheck user is login untill cookie expire so user not logout when refresh 
 
 
-export const AuthProvider = ({children})=>{
+// import { createContext,useState,useEffect,useContext } from "react";
 
-  let [user,setUser] = useState(null);  //intailly not login
+// import {fetechLogin} from "../assets/services/api";   //fetching the route 
 
-  let [checking,setChecking] = useState(true);
+// const AuthContext = createContext();  //creating empty container 
 
-  let [message,setMessage] = useState(""); //intailly no message 
 
-  let [errorMessage,setErrorMessage] = useState(""); //intailly no error
+// export const AuthProvider = ({children})=>{
 
-  useEffect(()=>{
+//   let [user,setUser] = useState(null);  //intailly not login
+
+//   let [checking,setChecking] = useState(true);
+
+//   let [message,setMessage] = useState(""); //intailly no message 
+
+//   let [errorMessage,setErrorMessage] = useState(""); //intailly no error
+
+//   useEffect(()=>{
     
-    fetechLogin()
-    .then((res)=>{
-      // console.log(res.data)
-      setMessage("user login successful");
-      setUser(res.data.user);
+//     fetechLogin()
+//     .then((res)=>{
+//       // console.log(res.data)
+//       setMessage("user login successful");
+//       setUser(res.data.user);
 
-    }).catch((err)=>{
-      // console.log(err.response.data)
-      setErrorMessage(err.response.data.message);
-      setUser(null);
+//     }).catch((err)=>{
+//       // console.log(err.response.data)
+//       setErrorMessage(err.response.data.message);
+//       setUser(null);
 
-    }).finally(()=>{
+//     }).finally(()=>{
 
-      setChecking(false);
+//       setChecking(false);
 
-    })
+//     })
 
-  },[]) 
-
-
-  const login = (userData)=>{setUser(userData)};  //every component can see weather login or not 
-
-  const logout = ()=>{setUser(null)};
-
-  if(checking)
-  {
-    return <h1>...loading</h1>
-  }
-
-  return (
-    <AuthContext.Provider value={{login,logout,user,checking,message,errorMessage}}>
-      {children}
-    </AuthContext.Provider>
-  )
+//   },[]) 
 
 
-}
+//   const login = (userData)=>{setUser(userData)};  //every component can see weather login or not 
 
-export const useAuth = ()=>useContext(AuthContext);
+//   const logout = ()=>{setUser(null)};
+
+//   if(checking)
+//   {
+//     return <h1>...loading</h1>
+//   }
+
+//   return (
+//     <AuthContext.Provider value={{login,logout,user,checking,message,errorMessage}}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+
+
+// }
+
+// export const useAuth = ()=>useContext(AuthContext);
+
+// context/AuthContext.js
+import { createContext, useState, useEffect, useContext } from "react";
+import Cookies from 'js-cookie';
+import { fetechLogin } from "../assets/services/api";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [checking, setChecking] = useState(true);
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+        // ✅ Read user from cookie
+        const currentUser = Cookies.get('currentUser');
+        
+        if (currentUser) {
+            try {
+                setUser(JSON.parse(currentUser));
+                setMessage("User verified");
+            } catch (error) {
+                console.error('Error parsing user cookie:', error);
+                setUser(null);
+            }
+        }
+        setChecking(false);
+    }, []);
+
+    const login = (userData) => {
+        setUser(userData);
+        setErrorMessage("");
+    };
+
+    const logout = () => {
+        setUser(null);
+        Cookies.remove('token');
+        Cookies.remove('currentUser');
+    };
+
+    return (
+        <AuthContext.Provider value={{ login, logout, user, checking, message, errorMessage }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
